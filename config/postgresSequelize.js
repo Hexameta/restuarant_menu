2879fe04340a160e3499ecfc9c2386b233a9670d
asif-mohmd <asifasifpsps@gmail.com>
2025-11-20 23:54:52 +0530

    nodemodules cache removed, basic setup 2

--- config/postgresSequelize.js
+++ config/postgresSequelize.js
@@ -8,13 +8,19 @@ const DBConn = new Sequelize(
   {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
-    dialect: "postgres", // Changed from mysql
+    dialect: "postgres",
     logging: false,
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true, // ⬅️ important for Neon
+        rejectUnauthorized: false, // avoid local cert issues
+      },
     },
   }
 );
