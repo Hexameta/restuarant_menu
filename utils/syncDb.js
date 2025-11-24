cc4f259081167683c2450923316b404c8dcd4d5a
mhdyaseen8841 <mhdyaseen.official@gmail.com>
2025-11-24 21:47:57 +0530

    changes

--- utils/syncDb.js
+++ utils/syncDb.js
@@ -13,7 +13,7 @@ const syncDatabase = async () => {
     // alter: true checks what is the current state of the table in the database
     // (which columns it has, what are their data types, etc), and then performs the
     // necessary changes in the table to make it match the model.
-    await DBConn.sync({ force: true });
+    await DBConn.sync({ alter: true });
     console.log("✅ Database synchronized successfully.");
     process.exit(0);
   } catch (error) {
