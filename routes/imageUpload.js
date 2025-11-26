ef260e385dca5d7a5c85e65fbb10a64864f653b9
riyaskh123 <riyaskh123@gmail.com>
2025-11-26 17:20:28 +0400

    upload update

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const {
   upload,
   uploadImage,
+  deletePdf,
+  uploadPdf,
 } = require("../controller/ImageController");
 const { deleteImage } = require("../controller/ImageController");
 
 router.post("/upload", upload.single("image"), uploadImage);
 router.post("/delete", deleteImage);
+router.post("/upload-pdf", upload.single("pdf"), uploadPdf);
+router.post("/delete-pdf", deletePdf);
 module.exports = router;
