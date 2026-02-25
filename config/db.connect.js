bb79723fbf5cf8362ebd9ccd78ae10696b39df37
mhdyaseen8841 <mhdyaseen.official@gmail.com>
2026-02-25 23:22:43 +0530

    mongodb connection debug

--- config/db.connect.js
+++ config/db.connect.js
@@ -1,14 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+let isConnected = false;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-    } catch (err) {
-        console.error(err);
-    }
-}
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
 
-module.exports = connectDB
+  try {
+    console.log("Connecting to MongoDB...");
+    console.log(process.env.DATABASE_URI)
+    const db = await mongoose.connect(process.env.DATABASE_URI, {
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      tls: true,
+    });
+
+    isConnected = db.connections[0].readyState;
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+};
+
+module.exports = connectDB;
